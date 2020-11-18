08c3957149162ee16d4407dd93bfd07b84389a6b
DarkDevCY <mkyriakou52@st.schools.ac.cy>
2020-11-18 06:01:26 +0200

    Added compare password for login

--- server.js
+++ server.js
@@ -27,47 +27,51 @@ app.post("/users", async (req, res) => {
     if (err) throw err;
     console.log("Shit can be added");
 
-  if (results < 1) {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      const user = {
-        name: req.body.username,
-        password: hashedPassword,
-        email: req.body.email,
-      };
-      users.push(user);
-      res.status(201).send();
-      console.log("Successfully added User!");
+    if (results < 1) {
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = {
+          name: req.body.username,
+          password: hashedPassword,
+          email: req.body.email,
+        };
+        users.push(user);
+        res.status(201).send();
+        console.log("Successfully added User!");
 
-      // Get data into DB
-      let username = user.name;
-      let pass = user.password;
-      let mail = user.email;
+        // Get data into DB
+        let username = user.name;
+        let pass = user.password;
+        let mail = user.email;
 
-      var sql =
-        "INSERT INTO `ghosted`(`username`,`email`,`password`) VALUES ('" +
-        username +
-        "','" +
-        mail +
-        "','" +
-        pass +
-        "')";
-      connection.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Shit added");
-      });
-    } catch {
-      res.status(500).send();
+        var sql =
+          "INSERT INTO `ghosted`(`username`,`email`,`password`) VALUES ('" +
+          username +
+          "','" +
+          mail +
+          "','" +
+          pass +
+          "')";
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Shit added");
+        });
+      } catch {
+        res.status(500).send();
+      }
+    } else {
+      console.log("Already exists");
     }
-  } else {
-    console.log("Already exists");
-  }
-});
+  });
 });
 
 app.post("/users/login", async (req, res) => {
-  const user = users.find((user) => user.name == req.body.username);
-  if (user == null) {
+  //const user = users.find((user) => user.name == req.body.username);
+
+  const userID = req.body.username;
+  //const password = await bcrypt.hash(req.body.password, 10);
+
+  /*if (user == null) {
     console.log("Cannot find user");
   }
   try {
@@ -80,7 +84,19 @@ app.post("/users/login", async (req, res) => {
     }
   } catch {
     res.status(500).send();
-  }
+  } */
+  var getDataLogin = "SELECT * FROM ghosted WHERE username='" + userID + "'";
+  console.log(userID);
+  console.log(req.body.password)
+  connection.query(getDataLogin, async function (err, results) {
+    console.log(err, results);
+    console.log(results[0].password)
+    if (await bcrypt.compare(req.body.password, results[0].password)) {
+      console.log('Hello');
+    } else {
+      console.log('FML')
+    }
+  });
 });
 
 app.listen(3000);
